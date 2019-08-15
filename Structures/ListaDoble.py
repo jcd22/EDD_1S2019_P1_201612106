88e1cdce3df28c7cc80c314329f2ed51c2e50935
jcd22 <chanaxsar3@hotmail.com>
2019-08-14 23:45:55 -0600

    practica1

--- Structures/ListaDoble.py
+++ Structures/ListaDoble.py
@@ -1,30 +1,116 @@
-class NodoDoble():
+import os, sys
 
 
-	def __init__(self,valor):
-		self.siguiente = None
-		self.anterior - None
-		selfl.valor - valor
+class Node():
 
-class ListaDoble():
+    def __init__(self, y, x):
+        self.next = None
+        self.prev = None
+        self.y = y
+        self.x = x
 
-	def __init__(self):
-		self,primerro = None
-		self.size = 0
+class DobleList():
 
-	def estaVacia():
-	    return self.size == 0	
+    def __init__(self):
 
-    def insertar_final(self, valor):
+        self.first = None
+        self.last = None
+        self.size = 0
 
-        if self.estaVacia():
-            self.primer = nevo
-    	else:	
-            temporal = self.primer #apnta al primero de la lista
-            while (temporal.siguiente!=None)
-                temporal = temporal.siguiente
-            #temporal apunta al ultimo elemento
-            #es decir que el sigiente de temporal es null
-            temporal.siguiente = nuevo
-            nuevo.anterior = temporal    
-        self.size = self.size + 1   
+    def empty(self):
+
+        return self.size == 0
+
+    def pop_last(self):
+       delated =  self.first
+       self.first = self.first.next
+       delated.next = None
+       self.size -= 1
+       print("pop("+str(delated.x)+ "," + str(delated.y) + ")")
+       return delated 
+
+
+    def insert_node(self, y, x):
+        newNode = Node(y, x)
+
+        if self.empty():
+            self.first = newNode
+            self.last = newNode
+            self.first.prev = None
+            self.last.next = None
+
+        else:
+             #temp = self.last
+            self.last.next = newNode
+            newNode.prev = self.last
+            self.last = newNode
+            self.last.next = None
+        self.size = self.size + 1
+
+    def printList(self):
+        print("adentro")
+        if self.empty() == False:
+            temp = self.first
+            i = 1
+
+        while ( temp != None):
+
+            print("("+str(temp.x)+ "," + str(temp.y) + ")")
+    
+            temp = temp.next
+            i += 1
+
+    def SnakeReport(self):
+
+        ContGraf = "digraph G {"+"\n"
+        if self.empty():
+            print("No Report is empy")
+
+        else:
+            i = 1
+            temp = self.first
+            ContGraf = ContGraf +"final"+"[label=\"NullCabeza\"]"+"\n"
+            ContGraf = ContGraf +"0"+"[label=\"Null\"]"+"\n"
+
+            while True:
+                ContGraf = ContGraf + str(i)+"[label=\"("+str(temp.x)+","+str(temp.y)+")\"]"+"\n"
+                temp = temp.next
+                if temp == None:
+                    break
+                else:
+                    i += 1
+
+            ContGraf = ContGraf + str(i)+" -> "+"final;"+"\n"
+            while True:
+                ContGraf = ContGraf+str(i)+" -> "+str(i-1)+";"+"\n"
+                ContGraf = ContGraf+str(i-1)+" -> "+str(i)+";"+"\n"
+                if i == 2:
+                    ContGraf = ContGraf+str(i-1)+" -> "+"0"+";"+"\n"
+                    break
+                else:
+                    i -= 1
+
+            ContGraf = ContGraf +"\n"+ "}"
+            new_file = open('SnakePos.dot','w')
+            new_file.write(ContGraf)
+            new_file.seek(0)
+            comando = " dot -Tpng  SnakePos.dot -o SnakePos.png"
+            os.system(comando)
+            os.system("SnakePos.png")
+
+
+
+"""lista = DobleList()
+lista.insert_node(3,4)
+lista.insert_node(4,5)
+lista.insert_node(5,5)
+lista.insert_node(6,5)
+lista.printList()
+lista.pop_last()
+lista.printList()
+lista.insert_node(7,5)
+lista.printList()
+lista.pop_last()
+lista.pop_last()
+lista.printList()"""
+
--- Structures/Menu201612106.py
+++ Structures/Menu201612106.py
@@ -1,13 +1,29 @@
-import time 
+import time
 import curses
 import ListaDobleEnlaz1
+import SnakeGame
+import ListaDoble
+from curses import KEY_RIGHT, KEY_LEFT, KEY_DOWN, KEY_UP
+from random import randint
+
 
 menu = ['Play', 'Scoreboard', 'User Selection', 'Reports', 'Bulk Loading','Exit']
+menu3 = ['SnakeReport', 'ScoreReport', 'ScoreBoardReport', 'UsersReport', 'Exit']
 Users = ['Juan', 'Pedro', 'Carlos', 'Fran', 'Walter','Tono']
 cir2 = ListaDobleEnlaz1.DobleList()
 temp = ListaDobleEnlaz1.NodeDoble("")
+juego = None
+list_juego = None
 size1 = None
 bulke = None
+WIDTH = 35
+HEIGHT = 20
+MAX_X = WIDTH - 2
+MAX_Y = HEIGHT - 2
+SNAKE_LENGTH = 5
+SNAKE_X = SNAKE_LENGTH + 1
+SNAKE_Y = 3
+TIMEOUT = 100
 """def BulkLoading(stdscr):
 
 	cir = ListaDobleEnlaz1.DobleList()
@@ -15,8 +31,12 @@ bulke = None
 	return bulk
 #otra instancia"""
 
-	
-	
+
+def RunGame(self):
+
+	curses.beep()
+	window = curses.newwin()
+
 
 def print_menu(stdscr, selected_row_idx,idMenu,tempo):
 
@@ -37,7 +57,7 @@ def print_menu(stdscr, selected_row_idx,idMenu,tempo):
 			else:
 				stdscr.attron(curses.color_pair(2))
 				stdscr.addstr(y, x, row)
-				stdscr.attroff(curses.color_pair(2))	
+				stdscr.attroff(curses.color_pair(2))
 
 		stdscr.refresh()
 
@@ -49,7 +69,7 @@ def print_menu(stdscr, selected_row_idx,idMenu,tempo):
 
 
 		for idx, row in enumerate(bulke):
-			
+
 			x = w//2 - row//2
 			y = h//2 - len(bulke)//2 + idx
 			#stdscr.addstr(y, x, row)
@@ -63,11 +83,31 @@ def print_menu(stdscr, selected_row_idx,idMenu,tempo):
 				stdscr.addstr(y, x, tempo.cont)
 				#stdscr.addstr(y, x, row)
 				stdscr.attroff(curses.color_pair(2))
-			#tempo = temp.next		
+			#tempo = temp.next
 	#time.sleep(10)
+	elif idMenu == 3:
+		
+		size3 = 5
+		new_list = list(range(size3))
+
+		for idx, row in enumerate(menu3):
+			x = w//2 - len(row)//2
+			y = h//2 - len(menu3)//2 + idx
+
+			if idx == selected_row_idx:
+				stdscr.attron(curses.color_pair(1))
+				stdscr.addstr(y, x, row)
+				#stdscr.addstr(y, x, row)
+				stdscr.attroff(curses.color_pair(1))
+			else:
+				stdscr.attron(curses.color_pair(2))
+				stdscr.addstr(y, x, row)
+				#stdscr.addstr(y, x, row)
+				stdscr.attroff(curses.color_pair(2))
+
 
 def movement_menu(stdscr):
-	
+
 	curses.curs_set(0)
 	curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
 	curses.init_pair(2, curses.COLOR_BLUE, curses.COLOR_WHITE)
@@ -94,20 +134,30 @@ def movement_menu(stdscr):
 			current_row_idx += 1
 		elif key == curses.KEY_ENTER or key in [10, 13]:#AL PRECIONAR ENTER
 
-			if current_row_idx == len(SelectedMenu)-1 and idMenu == 1: #exit
-				break
+			if current_row_idx == len(SelectedMenu)-1: #exit
+				if idMenu == 1:
+					break
+				elif idMenu == 3:
+					idMenu = 1
 
-			elif current_row_idx == len(SelectedMenu)-2 and idMenu == 1: #BulkLoad
+			elif current_row_idx == len(SelectedMenu)-2 : #BulkLoad
 				#new_bulk = BulkLoading(stdscr)
-				new_bulk = 1
-				cir2.BulkLoad()
-				stdscr.erase()
-				stdscr.addstr(0,0, "The BulkLoad has been completed")
-				stdscr.getch()
+				if idMenu == 1:
+					new_bulk = 1
+					cir2.BulkLoad()
+					stdscr.erase()
+					stdscr.addstr(0,0, "The BulkLoad has been completed")
+					stdscr.getch()
+
+				elif idMenu == 3:#SnakeReport
+					cir2.UsersReport() # reporte UserReport
 
 			elif current_row_idx == len(SelectedMenu)-3 and idMenu == 1: #Reports
-				cir2.UsersReport()
-				
+				#
+				idMenu = 3
+				new_tempo = None
+				SelectedMenu = menu3
+
 
 			elif current_row_idx == len(SelectedMenu)-4 and idMenu == 1:#player select
 				stdscr.erase()
@@ -122,10 +172,77 @@ def movement_menu(stdscr):
 				else :
 					stdscr.addstr(0,0,"Players have not been load")
 					stdscr.getch()
+
+			elif current_row_idx == len(SelectedMenu)-5:
+				if idMenu == 1:#ScoreBoard
+					print("Scoreboard")
+				elif idMenu == 3:
+					print("snakeReportess")
+					list_juego.SnakeReport()
+
+			elif current_row_idx == len(SelectedMenu)-6 and idMenu == 1: #Game selected
+				#corre la serpiente
+				stdscr.erase()
+				stdscr.clear()
+				juego = Game(stdscr)
+				juego1 = juego.mainSnake()
+				list_juego = juego.snake.snake_list
+				idMenu = 1
 				
+
 		print_menu(stdscr,current_row_idx,idMenu,new_tempo)
 		stdscr.refresh()
-	#time.sleep(3)
+
+class Game():
+	def __init__(self,stdscr):
+		self.stdscr = stdscr
+		self.stdscr = curses.newwin(HEIGHT, WIDTH, 0, 0)
+		self.stdscr.timeout(TIMEOUT)
+		curses.curs_set(0)
+		self.stdscr.border(0)
+		self.stdscr.keypad(1)
+		curses.noecho()
+		self.snake = SnakeGame.Snake(SNAKE_X, SNAKE_Y, self.stdscr)
+		self.food = SnakeGame.Food(self.stdscr, '+')
+
+		
+	def mainSnake(self):
+		
+		while True:
+			self.stdscr.clear()
+			self.stdscr.border(0)
+			self.snake.render()
+			self.food.render()
+
+			self.stdscr.addstr(0, 5, self.snake.score)
+			self.stdscr.addstr(0, 25,"level1")
+			if self.snake.hit_score >= 10:
+				self.stdscr.addstr(0, 25,"level2")
+
+			event = self.stdscr.getch()
+
+			if event == 27:
+				break
+
+			if event in [KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT]:
+				self.snake.change_direction(event)
+
+			if self.snake.head.x == self.food.x and self.snake.head.y == self.food.y:
+				self.snake.eat_food(self.food)
+
+			if event == 32:
+				key = -1
+				self.snake.snake_list.SnakeReport()
+				while key  != 32:
+					key = self.stdscr.getch()
+
+			self.snake.update()
+			if self.snake.collided:
+				print("colided")
+				self.snake.snake_list.SnakeReport()
+				break
+      
+
 
 def main(stdscr):
 
